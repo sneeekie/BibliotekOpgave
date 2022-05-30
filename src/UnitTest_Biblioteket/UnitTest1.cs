8bdccc851f03717917db7dc16d17ff8ca9692ddd
@adrian.istrefi.nuppnau <adrian.istrefi.nuppnau@lego.com>
2022-05-30 16:00:53 +0200

    added_testcase1

--- src/UnitTest_Biblioteket/UnitTest1.cs
+++ src/UnitTest_Biblioteket/UnitTest1.cs
@@ -1,10 +1,15 @@
+using System;
+using Xunit;
+using AdrianBibliotek;
 namespace UnitTest_Biblioteket;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Bibliotek_Hent_Bibliotek_True()
     {
-
+        Bibliotek bibnavn = new Bibliotek("sønderborg bibliotek");
+        string str = string.Format("velkommen til {0} - datoen i dag er: {1}", "sønderborg bibliotek", DateTime.Now.ToString());
+        Assert.Equal(bibnavn.HentBibliotek(), str);
     }
 }
